ab840e800d09888068dcab76dca3137bd9af9fe5
derulo taser <iih907@naver.com>
2024-01-20 23:28:13 +0900

    회원가입 서비스 작업 완료

--- src/main/java/shop/mtcoding/bank/service/UserService.java
+++ src/main/java/shop/mtcoding/bank/service/UserService.java
@@ -1,17 +1,15 @@
 package shop.mtcoding.bank.service;
 
-import lombok.Builder;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.bank.domain.user.User;
-import shop.mtcoding.bank.domain.user.UserEnum;
 import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.user.UserReqDto.JoinReqDto;
+import shop.mtcoding.bank.dto.user.UserRespDto.JoinRespDto;
 import shop.mtcoding.bank.handler.ex.CustomApiException;
 
 import java.util.Optional;
@@ -19,8 +17,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class UserService {
-
     private final Logger log = LoggerFactory.getLogger(getClass());
+
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
@@ -41,37 +39,5 @@ public class UserService {
         return new JoinRespDto(userPS);
     }
 
-    @Setter
-    @Getter
-    public static class JoinRespDto {
-        private Long id;
-        private String username;
-        private String fullname;
 
-        public JoinRespDto(User user) {
-            this.id = user.getId();
-            this.username = user.getUsername();
-            this.fullname = user.getFullname();
-        }
-    }
-
-    @Setter
-    @Getter
-    public static class JoinReqDto {
-        // 유효성 검사
-        private String username;
-        private String password;
-        private String email;
-        private String fullname;
-
-        public User toEntity(BCryptPasswordEncoder passwordEncoder) {
-            return User.builder()
-                    .username(username)
-                    .password(passwordEncoder.encode(password))
-                    .email(email)
-                    .fullname(fullname)
-                    .role(UserEnum.CUSTOMER)
-                    .build();
-        }
-    }
 }
---
+++ src/test/java/shop/mtcoding/bank/service/UserServiceTest.java
@@ -0,0 +1,66 @@
+package shop.mtcoding.bank.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserEnum;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.user.UserReqDto.JoinReqDto;
+import shop.mtcoding.bank.dto.user.UserRespDto.JoinRespDto;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+// Spring 관련 Bean들이 하나도 없는 환경!!
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest extends DummyObject {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Spy // 스파이는 Mock과 달리 Spring IoC에 있는 진짜 빈을 가져온다. 그리고 그걸 InjectMocks에 넣는다.
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @DisplayName("회원가입을 테스트한다.")
+    @Test
+    void 회원가입_test() {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("ssar");
+        joinReqDto.setPassword("1234");
+        joinReqDto.setEmail("ssar@nate.com");
+        joinReqDto.setFullname("쌀");
+
+        // stub 1
+        when(userRepository.findByUserName(any())).thenReturn(Optional.empty());
+//        when(userRepository.findByUserName(any())).thenReturn(Optional.of(new User()));
+
+        // stub 2
+        User ssar = newMockUser(1L, "ssar", "쌀");
+        when(userRepository.save(any())).thenReturn(ssar);
+
+
+
+        // when
+        JoinRespDto joinRespDto = userService.회원가입(joinReqDto);
+
+        // then
+        assertThat(joinRespDto.getId()).isEqualTo(1L);
+        assertThat(joinRespDto.getUsername()).isEqualTo("ssar");
+    }
+}
