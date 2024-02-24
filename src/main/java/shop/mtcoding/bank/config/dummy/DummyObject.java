54434026be11e7ba7fd4c48d26a37b8e089a9d06
derulo taser <iih907@naver.com>
2024-02-25 00:36:53 +0900

    @DataJpaTest 더미데이터 작업, autoincrement 초기화, 더티체킹

--- src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
+++ src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
@@ -2,6 +2,7 @@ package shop.mtcoding.bank.config.dummy;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.transaction.Transaction;
 import shop.mtcoding.bank.domain.transaction.TransactionEnum;
 import shop.mtcoding.bank.domain.user.User;
@@ -10,6 +11,75 @@ import shop.mtcoding.bank.domain.user.UserEnum;
 import java.time.LocalDateTime;
 
 public class DummyObject {
+
+    protected Transaction newWithdrawTransaction(Account account, AccountRepository accountRepository) {
+
+        account.withDraw(100L);
+
+        // Repository Test에서는 더티체킹 됨
+        // Controller Test에서는 더티체킹 안됨
+        if (accountRepository != null) {
+            accountRepository.save(account);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(account)
+                .depositAccount(null)
+                .withdrawAccountBalance(account.getBalance())
+                .depositAccountBalance(null)
+                .amount(100L)
+                .gubun(TransactionEnum.WITHDRAW)
+                .sender(account.getNumber() + "")
+                .receiver("ATM")
+                .build();
+
+        return transaction;
+    }
+
+    protected Transaction newDepositTransaction(Account account, AccountRepository accountRepository) {
+        account.deposit(100L);
+        // 더티체킹이 안되기 때문에
+        if (accountRepository != null) {
+            accountRepository.save(account);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(null)
+                .depositAccount(account)
+                .withdrawAccountBalance(null)
+                .depositAccountBalance(account.getBalance())
+                .amount(100L)
+                .gubun(TransactionEnum.DEPOSIT)
+                .sender("ATM")
+                .receiver(account.getNumber() + "")
+                .tel("01022227777")
+                .build();
+
+        return transaction;
+    }
+
+    protected Transaction newTransferTransaction(Account withdrawAccount, Account depositAccount, AccountRepository accountRepository) {
+
+        withdrawAccount.withDraw(100L);
+        depositAccount.deposit(100L);
+
+        // 더티체킹이 안되기 때문에
+        if (accountRepository != null) {
+            accountRepository.save(withdrawAccount);
+            accountRepository.save(depositAccount);
+        }
+
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(withdrawAccount)
+                .depositAccount(depositAccount)
+                .withdrawAccountBalance(withdrawAccount.getBalance())
+                .depositAccountBalance(depositAccount.getBalance())
+                .amount(100L)
+                .gubun(TransactionEnum.TRANSFER)
+                .sender(withdrawAccount.getNumber() + "")
+                .receiver(depositAccount.getNumber() + "")
+                .build();
+
+        return transaction;
+    }
 
     // 계좌 1111L 1000원
     // 입금 트랜잭션 -> 계좌 1100원 변경 -> 입금 트랜잭션 히스토리가 생성되어야 함.
--- src/main/java/shop/mtcoding/bank/domain/transaction/TransactionRepositoryImpl.java
+++ src/main/java/shop/mtcoding/bank/domain/transaction/TransactionRepositoryImpl.java
@@ -1,45 +1,50 @@
 package shop.mtcoding.bank.domain.transaction;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+
 import javax.persistence.TypedQuery;
-import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.repository.query.Param;
 
 interface Dao {
     List<Transaction> findTransactionList(@Param("accountId") Long accountId, @Param("gubun") String gubun,
                                           @Param("page") Integer page);
 }
 
-// Impl은 꼭 붙여줘야 하고, TransactionRepository가 앞에 붙어야 한다.
+// Impl은 꼭 붙여줘야 하고, TransactionRepository 가 앞에 붙어야 한다.
 @RequiredArgsConstructor
-public class TransactionRepositoryImpl implements Dao{
+public class TransactionRepositoryImpl implements Dao {
     private final EntityManager em;
 
+    // JPQL 이 무엇인지
+    // join fetch
+    // outer 조인을 해야 하는지
     @Override
     public List<Transaction> findTransactionList(Long accountId, String gubun, Integer page) {
-        // 동적 쿼리 (gubun 값을 가지고 동적쿼리 = DEPOSIT, WITHDRAW, ALL)
         // JPQL
         String sql = "";
-        sql += "select t from Transaction t";
+        sql += "select t from Transaction t ";
 
         if (gubun.equals("WITHDRAW")) {
-            sql += "join fetch t.withdraw wa ";
+            sql += "join fetch t.withdrawAccount wa ";
             sql += "where t.withdrawAccount.id = :withdrawAccountId";
         } else if (gubun.equals("DEPOSIT")) {
             sql += "join fetch t.depositAccount da ";
             sql += "where t.depositAccount.id = :depositAccountId";
         } else { // gubun = ALL
-            sql += "left join fetch t.withdrawAccount wa ";
-            sql += "left join fetch t.depositAccount da ";
+            sql += "left join t.withdrawAccount wa "; // 1,3,4,5
+            sql += "left join t.depositAccount da "; // 3,4,5
             sql += "where t.withdrawAccount.id = :withdrawAccountId ";
             sql += "or ";
             sql += "t.depositAccount.id = :depositAccountId";
         }
 
         TypedQuery<Transaction> query = em.createQuery(sql, Transaction.class);
-        
+
         if (gubun.equals("WITHDRAW")) {
             query = query.setParameter("withdrawAccountId", accountId);
         } else if (gubun.equals("DEPOSIT")) {
@@ -49,9 +54,10 @@ public class TransactionRepositoryImpl implements Dao{
             query = query.setParameter("depositAccountId", accountId);
         }
 
-        query.setFirstResult(page*5); // 5, 10, 15
+        query.setFirstResult(page * 5); // 5, 10, 15
         query.setMaxResults(5);
 
         return query.getResultList();
     }
+
 }
